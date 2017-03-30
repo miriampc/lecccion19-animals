e6c3070f230b00cec5140e99e1856cb6a087c086
Miriam Peralta <miiriampc@gmail.com>
2017-03-30 18:02:54 -0500

    creaccion de pagina animals maquetacion y funcionalidad

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+//window.addEventListener('load',function(){
+  var animal=document.getElementsByClassName('animal');
+  var select=document.getElementById('select');
+  select.onchange=function(){
+    if (select.value=="original") {
+      for (var i = 0; i < animal.length; i++) {
+        animal[i].classList.remove("blanco-negro");
+        animal[i].classList.remove("invertir-color");
+        animal[i].classList.remove("sepia");
+      }
+    }
+    if (select.value=="blanco-negro") {
+      for (var i = 0; i < animal.length; i++) {
+        animal[i].classList.add("blanco-negro");
+        animal[i].classList.remove("original");
+        animal[i].classList.remove("invertir-color");
+        animal[i].classList.remove("sepia");
+      }
+    }
+    if (select.value=="invertir-color") {
+      for (var i = 0; i < animal.length; i++) {
+        animal[i].classList.add("invertir-color");
+        animal[i].classList.remove("blanco-negro");
+        animal[i].classList.remove("original");
+        animal[i].classList.remove("sepia");
+      }
+    }
+    if (select.value=="sepia") {
+      for (var i = 0; i < animal.length; i++) {
+        animal[i].classList.add("sepia");
+        animal[i].classList.remove("blanco-negro");
+        animal[i].classList.remove("invertir-color");
+        animal[i].classList.remove("original");
+      }
+    }
+  }
+//});
